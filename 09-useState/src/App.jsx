3e1debca99a1bca100a24e3c422632d8d8c5f708
HaseebImd <haseeb.imdad@yahoo.com>
2026-07-10 17:30:04 +0500

    feat: replace number manipulation with form handling for user input

--- 09-useState/src/App.jsx
+++ 09-useState/src/App.jsx
@@ -1,24 +1,32 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [num, setNum] = useState(0);
-
-  function increaseNum() {
-    setNum(num + 1);
-  }
-  function decreaseNum() {
-    setNum(num - 1);
-  }
-  function jump5Num() {
-    setNum(num + 5);
+  const [title, settitle] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Form Submitted");
+    console.log();
+    settitle(e.target.value);
   }
 
   return (
     <div>
-      <h1>{num}</h1>
-      <button onClick={increaseNum}>increase</button>
-      <button onClick={decreaseNum}>decrease</button>
-      <button onClick={jump5Num}>Increas by 5</button>
+      <h2>{title}</h2>
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Enter your name"
+          onChange={(e) => {
+            settitle(e.target.value);
+          }}
+        />
+        <input type="password" placeholder="Enter your password" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
