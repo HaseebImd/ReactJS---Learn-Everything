dd12466266aab04a6c9084ab326470f377af4dac
HaseebImd <haseeb.imdad@yahoo.com>
2026-07-21 13:25:09 +0500

    feat: add axios dependency and implement data fetching in App component

--- 11-local-storage/src/App.jsx
+++ 11-local-storage/src/App.jsx
@@ -1,7 +1,8 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
 function App() {
   localStorage.setItem("name", "John Doe");
+  const [todos, setTodos] = useState([]);
 
   // Local storage has 4 methods: setItem(), getItem(), removeItem(), clear()
   // setItem() - adds data to local storage
@@ -10,14 +11,35 @@ function App() {
   // clear() - clears all data from local storage
 
   // How we store object and retrieve it from local storage
-  const user = {
-    name: "John Doe",
-    age: 30,
-    email: "johndoe@example.com",
-  };
-  localStorage.setItem("user", JSON.stringify(user));
-  const retrievedUser = JSON.parse(localStorage.getItem("user"));
-  return <div></div>;
+  // const user = {
+  //   name: "John Doe",
+  //   age: 30,
+  //   email: "johndoe@example.com",
+  // };
+  // localStorage.setItem("user", JSON.stringify(user));
+  // const retrievedUser = JSON.parse(localStorage.getItem("user"));
+  // async function getData() {
+  //   const response = await fetch("https://jsonplaceholder.typicode.com/todos")
+  //     .then((response) => response.json())
+  //     .then((response) => setTodos(response));
+  //   console.log(todos);
+  // }
+
+  async function getData() {
+    await axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => setTodos(response.data))
+      .catch((error) => console.log(error));
+  }
+
+  return (
+    <div>
+      <button onClick={getData}>Get Data</button>
+      {todos.map((todo, index) => {
+        return <h3 key={index}>{todo.title}</h3>;
+      })}
+    </div>
+  );
 }
 
 export default App;
